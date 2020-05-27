55debfc08b040c94f78bbef045b91128f0fbeaad
Mostefaoui Mohamed Anes <m.anas@outlook.com>
2020-05-27 10:35:22 +0300

    add env sourcing to down script

--- docker-compose-down.sh
+++ docker-compose-down.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source docker-compose-env.sh
 docker-compose -p "network" -f ./compose/network-compose.yaml down
 docker-compose -p "container-manager" -f ./compose/container-manager-compose.yaml down
 docker-compose -p "media" -f ./compose/media-compose.yaml down
